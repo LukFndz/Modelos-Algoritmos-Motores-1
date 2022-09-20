797a43d58a4b2123c6a60beb4eb8c211ed4b8388
Luca Fernandez <56230255+LukFndz@users.noreply.github.com>
2022-09-19 22:31:36 -0300

    Factory

--- Only Road/Assets/Scripts/Enemy/EnemySpawner.cs
+++ Only Road/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -6,10 +6,13 @@ public class EnemySpawner : MonoBehaviour
 {
     public Enemy enemy;
     ObjectPool<Enemy> _pool;
+    Factory<Enemy> _factory;
     [SerializeField] private List<Transform> spawnPoint;
+
     void Start()
     {
-        _pool = new ObjectPool<Enemy>(Factory, Enemy.TurnOn, Enemy.TurnOff, 8);
+        _factory = new Factory<Enemy>(enemy);    
+        _pool = new ObjectPool<Enemy>(_factory.Get, Enemy.TurnOn, Enemy.TurnOff, 8);
     }
 
     // Update is called once per frame
@@ -19,12 +22,7 @@ public class EnemySpawner : MonoBehaviour
         {
             var e = _pool.GetObject();
             e.Create(_pool);
-            e.transform.position = spawnPoint[Random.Range(0, 6)].position;
+            e.transform.position = spawnPoint[Random.Range(0, 5)].position;
         }
-    }
-
-    public Enemy Factory()
-    {
-        return Instantiate(enemy);
     }
 }
---
+++ Only Road/Assets/Scripts/Factory/Factory.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory<T> where T : MonoBehaviour
+{
+    T _prefab;
+
+    public Factory(T prefab)
+    {
+        _prefab = prefab;
+    }
+
+    public T Get()
+    {
+        return GameObject.Instantiate(_prefab);
+    }
+}
