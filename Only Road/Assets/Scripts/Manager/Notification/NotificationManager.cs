295d0f432dacef279163a4e0d6721a69b612304c
Luca Fernandez <56230255+LukFndz@users.noreply.github.com>
2022-11-04 00:39:37 -0300

    Notification System

---
+++ Only Road/Assets/Scripts/Manager/Notification/NotificationManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Notifications.Android;
+
+public class NotificationManager : MonoBehaviour
+{
+    private void Start()
+    {
+        var notificationChannel = new AndroidNotificationChannel()
+        {
+            Id = "reminder_notif",
+            Name = "Reminder Notification",
+            Description = "Channel for reminders notification",
+            Importance = Importance.High
+        };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(notificationChannel);
+
+        var notif = new AndroidNotification();
+        notif.Title = "Only Road";
+        notif.Text = "¡Hay autos que esquivar!";
+        notif.SmallIcon = "icon_remindersmall";
+        notif.LargeIcon = "icon_reminderbig";
+        notif.FireTime = System.DateTime.Now.AddHours(2);
+
+        AndroidNotificationCenter.SendNotification(notif, "reminder_notif");
+    }
+}
